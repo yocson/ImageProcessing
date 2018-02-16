8256030e7f2de83684710fd58ece9bbbb0ac35fc
Yocson Wang <yocson@outlook.com>
2018-02-16 16:17:42 -0500

    finish hw2

--- IMHW2/edge_detection.py
+++ IMHW2/edge_detection.py
@@ -2,7 +2,7 @@ from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage, signal
-from math import sqrt, pi
+from math import sqrt, pi, isnan
 
 def gaussian(x, mu, sig):
     return 1./(sqrt(2.*pi)*sig)*np.exp(-np.power((x - mu)/sig, 2.)/2)
@@ -73,10 +73,13 @@ def get_direction(Eo):
     """
     dir_img = np.array([])
     for x in np.nditer(Eo):
+        if (isnan(x)):
+            direction = 90
+            dir_img = np.append(dir_img, direction)
+            continue
         x = x / pi * 180
         if (x < 0):
             x = x + 180
-        direction = 0
         if ((x >= 0 and x < 22.5) or (x > 157.5 and x <= 180)): 
             direction = 0
         elif (x >= 22.5 and x < 67.5):
@@ -90,12 +93,12 @@ def get_direction(Eo):
     return dir_img.astype(int).reshape(Eo.shape[0], Eo.shape[1])
 
 def checkBoundary(im, jm, ip, jp, Es):
-    if (im < 0 or ip > Es.shape[0] or jm < 0 or jp > Es.shape[1]):
+    if (im < 0 or ip >= Es.shape[0] or jm < 0 or jp >= Es.shape[1]):
         return False
     else:
         return True
 
-def nonmax_suppression(Es, Eo):
+def nonmax_suppression(Es, Eo, dir_img):
     """Operate algorithm of nonmax_suppression
 
     To produce 1-pixel wide edge
@@ -103,11 +106,11 @@ def nonmax_suppression(Es, Eo):
     Args:
         Es: strength iamge
         Eo: orientation image
+        dir_img: direction with 0 45 90 135
 
     Returns:
         suppressed image
     """
-    dir_img = get_direction(Eo)
 
     suppressed_img = np.zeros_like(Es)
     it = np.nditer(Es, flags=['multi_index'])
@@ -115,12 +118,14 @@ def nonmax_suppression(Es, Eo):
         i = it.multi_index[0]
         j = it.multi_index[1]
         im, jm, ip, jp = get_neighbor_of_index(i, j, dir_img)
-        if (checkBoundary(im, jm, ip, jp, Es) and it[0] >= Es[im, jm] and it[0] >= Es[ip, jp]):
-            suppressed_img[i, j] = it[0]
+        if (checkBoundary(im, jm, ip, jp, Es)):
+            if (it[0] >= Es[im, jm] and it[0] >= Es[ip, jp]):
+                suppressed_img[i, j] = it[0]
         it.iternext()
+
     return suppressed_img
         
-def get_neighbor_of_index(i, j, dir_img):
+def get_neighbor_of_index(i, j, dir_img, direct=0):
     """get neighbors' indices
 
     Find neighbors of i, j along the edge direction
@@ -133,21 +138,44 @@ def get_neighbor_of_index(i, j, dir_img):
         Two neighbors's indices, [im, jm] and [jp, jp]
     """
     direction = dir_img[i, j]
-    if (direction == 0):
-        im, jm = i-1, j
-        ip, jp = i+1, j
-    elif (direction == 45):
-        im, jm = i-1, j+1
-        ip, jp = i+1, j-1
-    elif (direction == 90):
-        im, jm = i, j-1
-        ip, jp = i, j+1
+    if (direct == 0):
+        if (direction == 0):
+            im, jm = i, j-1
+            ip, jp = i, j+1
+        elif (direction == 45):
+            # im, jm = i-1, j+1
+            # ip, jp = i+1, j-1
+            im, jm = i-1, j-1
+            ip, jp = i+1, i+1
+        elif (direction == 90):
+            im, jm = i-1, j
+            ip, jp = i+1, j
+        else:
+            # im, jm = i-1, j-1
+            # ip, jp = i+1, i+1
+            im, jm = i-1, j+1
+            ip, jp = i+1, j-1
     else:
-        im, jm = i-1, j-1
-        ip, jp = i+1, i+1
+        if (direction == 0):
+            im, jm = i-1, j
+            ip, jp = i+1, j
+        elif (direction == 45):
+            im, jm = i-1, j+1
+            ip, jp = i+1, j-1
+            # im, jm = i-1, j-1
+            # ip, jp = i+1, i+1
+        elif (direction == 90):
+            im, jm = i, j-1
+            ip, jp = i, j+1
+        else:
+            # im, jm = i-1, j+1
+            # ip, jp = i+1, j-1
+            im, jm = i-1, j-1
+            ip, jp = i+1, i+1
+
     return im, jm, ip, jp
 
-def track_edge(i, j, In, dir_img, visited_img, ti):
+def track_edge(i, j, In, dir_img, visited_img, edge_img, ti):
     """chain all edges
 
     From point i, j, check whether it is greater than ti,
@@ -159,14 +187,15 @@ def track_edge(i, j, In, dir_img, visited_img, ti):
         dir_img: with direction of degree 0, 45, 90, 135
         ti: low thresholds
     """
-    im, jm, ip, jp = get_neighbor_of_index(i, j, dir_img)
-    if (In[im, jm] > ti):
-        visited_img[im, jm] = 1
-        track_edge(im, jm, In, dir_img, visited_img, ti)
-    if (In[ip, jp] > ti):
-        visited_img[ip, jp] = 1
-        track_edge(ip, jp, In, dir_img, visited_img, ti)
-    return
+    im, jm, ip, jp = get_neighbor_of_index(i, j, dir_img, 1)
+    visited_img[i, j] = 1
+    if (im >= 0 and jm < In.shape[1] and In[im, jm] > ti and not visited_img[im, jm]):
+        edge_img[im, jm] = In[im, jm]
+        visited_img, edge_img = track_edge(im, jm, In, dir_img, visited_img, edge_img, ti)
+    if (ip < In.shape[0] and jp >= 0  and In[ip, jp] > ti and not visited_img[ip, jp]):  
+        edge_img[ip, jp] = In[ip, jp]
+        visited_img, edge_img = track_edge(ip, jp, In, dir_img, visited_img, edge_img, ti)
+    return visited_img, edge_img
     
 def hystersis_threshold(In, dir_img, ti, th):
     """Operate HYSTERIS_THRESHOLD
@@ -183,31 +212,29 @@ def hystersis_threshold(In, dir_img, ti, th):
         images with edge chains
     """
     visited_img = np.zeros_like(In)
+    edge_img = np.zeros_like(In)
 
     it = np.nditer(In, flags=['multi_index'])
     while not it.finished:
-        if (it[0] < th): 
-            continue
-        else:
+        if (it[0] > th): 
             i = it.multi_index[0]
             j = it.multi_index[1]
-            if (visited_img[i, j] == 1):
-                continue
-            visited_img[i, j] == 1
-            track_edge(i, j, In, dir_img, visited_img, ti)
+            if (visited_img[i, j] == 0): 
+                edge_img[i, j] = it[0]
+                visited_img, edge_img = track_edge(i, j, In, dir_img, visited_img, edge_img, ti)
         it.iternext()
-    return visited_img
+    return edge_img
 
 if __name__ == '__main__':
     image = io.imread('Flowers.jpg')
-    Es, Eo = canny_enhancer(image, 0.9)
-    
-    print(Es)
-    print(Eo)
-
+    Es, Eo = canny_enhancer(image, 1)
+    print("ENHACER")
+    dir_img = get_direction(Eo)
+    In = nonmax_suppression(Es, Eo, dir_img)
+    print("NONMAX")
+    edge_img = hystersis_threshold(In, dir_img, 20 ,60)
+    io.imshow(edge_img, cmap="gray")
     plt.show()
-    
-
     
 
 
