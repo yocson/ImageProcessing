8f7c3085e14932924a5f21eb7c17a60d41a69a54
Yocson Wang <yocson@outlook.com>
2018-03-30 23:47:38 -0400

    add comment

--- IMHW5/background.py
+++ IMHW5/background.py
@@ -10,7 +10,6 @@ from scipy import ndimage, signal
 from scipy.linalg import eigh as largest_eigh
 from scipy.spatial import distance
 from skimage import io
-from sklearn import preprocessing
 from skimage.morphology import square, opening, closing
 
 class Codeword:
@@ -35,38 +34,53 @@ def brightness(I, aux, alpha, beta):
     
 
 def create_codebook(videopath, e1, alpha, beta):
+    # read all video frames in
     frames = [join(videopath, f) for f in listdir(videopath) if isfile(join(videopath, f))]
+    # sort frames
+    frames.sort()
+    # create codebook for the whole theme
     code_book = np.zeros_like(io.imread(frames[0]))
     updated = False
-    for t, fr in enumerate(frames):
+    # for every frame
+    # note: index start from 0, if we use t, we need to add one to it.
+    for index, fr in enumerate(frames):
+        t = index + 1
         img = io.imread(fr)
         it = np.nditer(img, flags=['multi_index'])
+        # for every pixel
         while not it.finished:
             i = it.multi_index[0]
             j = it.multi_index[1]
+            # first frame, set every pixel's codebook to an empty list
             if (t == 0):
                 code_book[i, j] = []
-
+            # set x = (R, G, B), I = sqrt(R^2+G^2+B^2)
             xt = [it[0], it[1], it[2]]
             I = sqrt(it[0]**2 + it[1]**2 + it[2]**2)
-
+            # iterate over the codewords at this pixel
             for ix, cw in enumerate(code_book[i, j]):
-                if (colordis(xt, cw.vec) < e1 and brightness(I, cw.aux, alpha, beta)):
+                # match
+                if (colordis(xt, cw.vec) <= e1 and brightness(I, cw.aux, alpha, beta)):
                     auxm = code_book[i, j][ix].aux
                     fm = auxm[2]
                     vm = code_book[i, j][ix].vec
-                    code_book[i, j][ix].vec = [(fm*vm[0] + xt[0])/(fm + 1), (fm*vm[1] + xt[1])/(fm + 1), (fm*vm[2] + xt[2])/(fm + 1)]
-                    code_book[i, j][ix].aux = [min(I, auxm[0]), max(I,auxm[1]), fm+1, max(auxm[3], t-auxm[5]), auxm[4], t + 1]
+                    # update vector of RGB
+                    code_book[i, j][ix].vec = [(fm * vm[0] + xt[0])/(fm + 1), (fm * vm[1] + xt[1])/(fm + 1), (fm * vm[2] + xt[2])/(fm + 1)]
+                    # update aux (Im, IM, fm, lm, pm, qm)
+                    code_book[i, j][ix].aux = [min(I, auxm[0]), max(I,auxm[1]), fm + 1, max(auxm[3], t - auxm[5]), auxm[4], t]
                     updated = True
                     break
 
+            # if not matched
             if (updated == False):
                 # create new code word
                 vl = xt
-                auxl = [I, I, 1, t, t + 1, t + 1]
+                auxl = [I, I, 1, t - 1, t, t]
                 code_book[i, j].append(Codeword(vl, auxl))
                 
-            if (t == len(frames) - 1):
+            # if the current frame is the last one, we need to wrap lambda for current pixel
+            if (t == len(frames)):
+                # for every codeword in this codebook
                 for ix, cw in enumerate(code_book[i, j]):
                     auxi = code_book[i, j][ix].aux
                     code_book[i, j][ix].aux[3] = max(auxi[3], len(frames)-auxi[5]+auxi[4]-1)
@@ -78,10 +92,12 @@ def create_codebook(videopath, e1, alpha, beta):
 def compress_codebook(code_book, N):
     it = np.nditer(code_book, flags=['multi_index'])
     Tm = N / 2
+    # iterate over the whole codebook
     while not it.finished:
         i = it.multi_index[0]
         j = it.multi_index[1]
         new_code_words = []
+        # iterate over all codeword in current pixel
         for cw in code_book[i, j]:
             if (cw.aux[3] <= Tm):
                 new_code_words.append(cw)
@@ -94,10 +110,11 @@ def detect_foreground(image, code_book, e2, alpha, beta):
     it = np.nditer(img, flags=['multi_index'])
     BGS = np.zeros_like(img)
     match = False
+    # iterate all pixel
     while not it.finished:
         i = it.multi_index[0]
         j = it.multi_index[1]
-    
+        # set x = (R, G, B), I = sqrt(R^2+G^2+B^2)
         xt = [it[0], it[1], it[2]]
         I = sqrt(it[0]**2 + it[1]**2 + it[2]**2)
 
@@ -105,7 +122,7 @@ def detect_foreground(image, code_book, e2, alpha, beta):
             if (colordis(xt, cw.vec) < e2 and brightness(I, cw.aux, alpha, beta)):
                 match = True
                 break
-
+        # if is background, set to black, else white
         if (match):
             BGS[i, j] = 0
         else:
